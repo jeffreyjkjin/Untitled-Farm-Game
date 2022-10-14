d7ec9f4a5ec073c5e0c02de77c8ed6bb238d0fcd
jeffreyjkjin <93238445+jeffreyjkjin@users.noreply.github.com>
2022-10-14 14:29:33 -0700

    Created game window in main.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,17 +1,25 @@
 package com.example;
 
+import javax.swing.JFrame;
+
 /**
  * Hello world!
  */
 public final class App {
-    private App() {
-    }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Untitled Farm Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
         System.out.println("Hello World!");
     }
 }
