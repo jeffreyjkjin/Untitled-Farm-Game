4a50b51024ee76c69d49c0c10dd084c5572b4047
jeffreyjkjin <93238445+jeffreyjkjin@users.noreply.github.com>
2022-10-14 14:38:27 -0700

    Created startGameThread and stub for run methods in GamePanel.

--- src/main/java/com/example/GamePanel.java
+++ src/main/java/com/example/GamePanel.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;
     final int scale = 3;
 
@@ -15,10 +15,23 @@ public class GamePanel extends JPanel{
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
 
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
     }
 }
