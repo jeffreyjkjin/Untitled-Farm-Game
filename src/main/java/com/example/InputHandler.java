419da0250b465150583709b0d7bdbe8967d729c5
Jeffrey Jin <jjj9@sfu.ca>
2022-10-15 02:00:17 -0700

    Created InputHandler class; implemented keyPressed and keyReleased methods.

---
+++ src/main/java/com/example/InputHandler.java
@@ -0,0 +1,64 @@
+package com.example;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InputHandler implements KeyListener {
+
+    public boolean up, down, left, right;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        
+        switch(keyCode) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_KP_UP:
+                up = true;
+                break;
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_KP_LEFT:
+                left = true;
+                break;
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_KP_DOWN:
+                down = true;
+                break;
+            case KeyEvent.VK_D:
+            case KeyEvent.VK_KP_RIGHT:
+                right = true;
+                break;
+            default:
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        
+        switch(keyCode) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_KP_UP:
+                up = false;
+                break;
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_KP_LEFT:
+                left = false;
+                break;
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_KP_DOWN:
+                down = false;
+                break;
+            case KeyEvent.VK_D:
+            case KeyEvent.VK_KP_RIGHT:
+                right = false;
+                break;
+            default:
+        }
+    }
+    
+}
