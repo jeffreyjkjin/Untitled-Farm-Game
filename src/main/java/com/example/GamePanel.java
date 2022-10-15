2de34c8e13982682ac13a832ca7f61993500c65e
Jeffrey Jin <jjj9@sfu.ca>
2022-10-15 02:03:34 -0700

    Implemented update method in GamePanel.

--- src/main/java/com/example/GamePanel.java
+++ src/main/java/com/example/GamePanel.java
@@ -17,13 +17,19 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    InputHandler input = new InputHandler();
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
-
+        this.addKeyListener(input);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -40,7 +46,18 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-
+        if (input.up) {
+            playerY -= playerSpeed;
+        }
+        else if (input.left) {
+            playerX -= playerSpeed;
+        }
+        else if (input.down) {
+            playerY += playerSpeed;
+        }
+        else if (input.right) {
+            playerX += playerSpeed;
+        }       
     }
 
     public void paintComponent(Graphics graphic) {
@@ -48,7 +65,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D graphic2 = (Graphics2D)graphic;
         graphic2.setColor(Color.green);
-        graphic2.fillRect(100, 100, tileSize, tileSize);
+        graphic2.fillRect(playerX, playerY, tileSize, tileSize);
 
         graphic2.dispose();
     }
