47e3eaac6a129b8048e7ddabfd72c037984d1adb
jeffreyjkjin <93238445+jeffreyjkjin@users.noreply.github.com>
2022-10-21 10:32:41 -0700

    Removed temporary player attributes in GamePanel.

--- src/main/java/app/GamePanel.java
+++ src/main/java/app/GamePanel.java
@@ -4,21 +4,21 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import tile.*;
-
-import object.SuperObject;
 
 import javax.swing.JPanel;
 
 import entity.Player;
 import object.SuperObject;
+import tile.Map;
 import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
+    // Tiles
     final int originalTileSize = 16;
     final int scale = 3;
-
     public final int tileSize = originalTileSize * scale;
+
+    // Screen
     public final int maxScreenCol = 16;
     public final int maxScreenRow = 12;
     public final int screenWidth = tileSize * maxScreenCol;
@@ -26,6 +26,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     final int FPS = 60;
 
+    // System
     public TileManager tileM = new TileManager(this);
     public Map map = new Map(this, "/levels/levelTest1.txt");
     InputHandler input = new InputHandler(this);
@@ -35,11 +36,6 @@ public class GamePanel extends JPanel implements Runnable {
     public Player player = new Player(this, input);
     public SuperObject obj[] = new SuperObject[10]; // 10 slots for object
     public AssetSetter aSetter = new AssetSetter(this);
-
-    
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -66,27 +62,17 @@ public class GamePanel extends JPanel implements Runnable {
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
-        long timer = 0;
-        int FPSCount = 0;
         
         while (gameThread != null) {
             currentTime = System.nanoTime();
 
             delta += (currentTime - lastTime) / drawInterval;
-            timer += (currentTime - lastTime);
             lastTime = currentTime;
 
             if (delta >= 1) {
                 update();
                 repaint();
                 delta--;
-                FPSCount++;
-            }
-
-            if (timer >= 1000000000) {
-                System.out.println("FPS: " + FPSCount);
-                FPSCount = 0;
-                timer = 0;
             }
         }
     }
